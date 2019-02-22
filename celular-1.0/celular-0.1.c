ad5782655df77093f5474540ab205ff1c4d9bd60
Note45 <antonion.2048@gmail.com>
2019-02-22 14:49:49 -0300

    Interface grafica basica

--- celular-1.0/celular-0.1.c
+++ celular-1.0/celular-0.1.c
@@ -8,44 +8,55 @@ void gotoxy(int x, int y){
 
 int main() {
 	int x;
-	int y;
 	
-	//criando interface
 	system("cls");
 	//topo
-	for(x = 0; x < 35; x++) {
+	int y;
+	for(x = 0; x < 37; x++) {
 		gotoxy(0, 0);
 		printf("%c", 219);
 	}
 	
 	//esquerda e direita
-	for(x = 0; x < 25; x++) {
+	for(x = 1; x < 25; x++) {
 		gotoxy(0, x);
 		printf("%c", 219);
-		gotoxy(37, x);
+		gotoxy(38, x);
 		printf("%c\n", 219);
 	}
 
 	//parte de baixo
-	for(x = 0; x < 37; x++) {
+	for(x = 0; x < 38; x++) {
 		gotoxy(0, x);
 		printf("%c",219);
 	}
 	
-	//divisôes
-	for(x = 2; x < 37; x++) {
-		gotoxy(x, 16);
+	//espaçamento superior
+	for(x = 2; x < 38; x++) {
+		gotoxy(x, 19);
 		printf("%c", 220);
 	}
 	
-	for(x = 17; x <=25; x++) {
-		gotoxy(11, x);
+	//divisorias
+	for(x = 20; x < 25; x++) {
+		gotoxy(12, x);
 		printf("%c", 219);
 	}
 	
-	for(x = 17; x <=25; x++) {
-		gotoxy(22, x);
+	for(x = 20; x < 25; x++) {
+		gotoxy(25, x);
 		printf("%c", 219);
 	}
 	
-}
+	//comandos da tela
+	gotoxy(3, 22);
+	printf("1-vStore\n\n\n");
+	
+	gotoxy(14, 22);
+	printf("2-My Apps\n\n\n");
+	
+	gotoxy(29, 22);
+	printf("3-Rum\n\n\n");
+	
+	return 0;
+}                  
